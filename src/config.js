53289ab97e9993bbfe625a6b05fe0a0539eb4d68
iceeburr <iceeburr@gmail.com>
2024-09-10 03:40:04 +0300

    fix api link

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 export const production = false;
 
 export default {
-  api: "https://ggshelpice.onrender.com",
+  api: "https://ggshelpice-vwiv.onrender.com/",
   h_captcha_key: production
     ? "495be111-f6a7-4ca5-9b8f-d0149998a742"
     : "20000000-ffff-ffff-ffff-000000000002",
